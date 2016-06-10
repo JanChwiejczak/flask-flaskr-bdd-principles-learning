a902c1c5e8b64332f8de98e19e154d0a7fc7e7fc
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-06-10 12:31:15 +0100

    add app routes & logic for log in and log out

--- flaskr.py
+++ flaskr.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, request, flash, render_template, session, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def helloworld():
+
+@app.route('/Hello')
+def hello():
     return 'Hello World!'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+        return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
+
+
+@app.route('/index')
+def index():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
