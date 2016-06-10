1390d0cd418069e08ce252af6d0ced8067c6cf58
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-06-10 23:57:08 +0100

    add db functions and changed index and add routes

--- flaskr.py
+++ flaskr.py
@@ -1,15 +1,43 @@
-from flask import Flask, request, flash, render_template, session, redirect, url_for, abort
+from flask import Flask, request, flash, render_template, session, redirect, url_for, abort, g
+import sqlite3
 
 # Configuration
 
 
 app = Flask(__name__)
 app.config.update(
-    DATABASE='',
+    DATABASE='flaskr.db',
     USERNAME='admin',
     PASSWORD='admin',
     SECRET_KEY='change_me'
     )
+
+#connect to database
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE_PATH'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+#create the database
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+            db.commit()
+
+#open the database connection
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+#close the database connection
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
 
 @app.route('/hello')
 def hello():
@@ -40,12 +68,19 @@ def logout():
 
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    db = get_db()
+    cur = db.execute('SELECT title, text FROM entries ORDER BY id DESC ')
+    entries = cur.fetchall()
+    return render_template('index.html', entries=entries)
 
 
 @app.route('/add', methods=['POST'])
 def add():
     if session.get('logged_in'):
+        db = get_db()
+        cur = db.execute('INSERT INTO entries (title, text) VALUES (?, ?)',
+                         [request.form['title'], request.form['text']])
+        db.commit()
         flash('New entry was successfully posted')
     else:
         abort(401)
